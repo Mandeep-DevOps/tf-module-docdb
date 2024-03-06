32169b586acd5cadc2f136a41c78a705111ee526
SVR Kalluru <raghuk.vit@gmail.com>
2024-03-06 08:44:48 +0530

    Enabling DocDB

---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "engine" {}
+variable "engine_version" {}
+variable "instance_class" {}
+variable "env" {}
+variable "subnets" {}
+variable "vpc_id" {}
+variable "sg_cidrs" {}
+variable "kms" {}
+variable "parameter_group_family" {}
+variable "instance_count" {}
+variable "project_name" {}
+variable "tags" {}
